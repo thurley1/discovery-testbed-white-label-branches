4f42817b2e6ceb4d1d18b097a198a4570a3b8226
Todd Hurley <thurley74@gmail.com>
2026-03-11 19:10:37 -0400

    chore(initech): add tenant configuration

---
+++ src/VariantTestbed.Infrastructure/InitechConfig.cs
@@ -0,0 +1,7 @@
+namespace VariantTestbed.Infrastructure.Config;
+
+public static class InitechConfig
+{
+    public const string TenantId = "initech-001";
+    public const string DisplayName = "Initech Corporation";
+}
